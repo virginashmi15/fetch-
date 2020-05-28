38c50888045bc19c73e09a763d53b391ea48e358
virginashmi15 <65062793+virginashmi15@users.noreply.github.com>
2020-05-28 01:27:29 -0700

    Fetch + JSON

---
+++ fetch/fetch.js
@@ -0,0 +1,27 @@
+const fetchData = async ()=>{
+    const response = await fetch('https://www.learningcontainer.com/wp-content/uploads/2019/10/Sample-JSON-file-with-multiple-records-download.json');
+    const data = await response.json()
+    await data.forEach(item => {
+        const domElement = document.getElementById('table-body-content');
+        const row1 = domElement.insertRow();
+        const cell1 = row1.insertCell(0);
+        var id  = document.createTextNode(item.id)
+        cell1.appendChild(id);
+        const cell2 = row1.insertCell(1);
+        const name = document.createTextNode(item.name);
+        cell2.appendChild(name);
+        const cell3 = row1.insertCell(2);
+        const email = document.createTextNode(item.email);
+        cell3.appendChild(email);
+        const cell4 = row1.insertCell(2);
+        const phone = document.createTextNode(item.phone);
+        cell4.appendChild(phone)
+    });
+    
+}
+
+
+
+const loaderFunc = setTimeout(()=> fetchData(),2000);
+
+
